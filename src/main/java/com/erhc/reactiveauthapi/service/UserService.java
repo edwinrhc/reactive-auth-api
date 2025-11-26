a3845574aa0c12e0579539764ff6ff58209dcca7
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-11-25 23:56:47 -0500

    feat(service): implement registration with DTO password encoding - Update register method to accept RegisterDTO instead of User entity. - Integrate PasswordEncoder for secure storage. - Replace generic exception with EmailAlreadyExistsException - Refactor dependency injection to constructor-based

--- src/main/java/com/erhc/reactiveauthapi/service/UserService.java
+++ src/main/java/com/erhc/reactiveauthapi/service/UserService.java
@@ -1,6 +1,11 @@
 package com.erhc.reactiveauthapi.service;
 
+import com.erhc.reactiveauthapi.dto.RegisterDTO;
+import com.erhc.reactiveauthapi.exception.EmailAlreadyExistsException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Flux;
@@ -9,18 +14,32 @@ import reactor.core.publisher.Mono;
 import com.erhc.reactiveauthapi.entity.User;
 import com.erhc.reactiveauthapi.repository.UserRepository;
 
+@Slf4j
 @Service
+@AllArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository repo;
 
-    public Mono<User> register(User user){
-        return repo.findByEmail(user.getEmail())
+    private final UserRepository repo;
+    private PasswordEncoder passwordEncoder;
+
+    public Mono<User> register(RegisterDTO dto){
+
+        return repo.findByEmail(dto.getEmail())
                 .flatMap(existing ->
-                        Mono.<User> error(new RuntimeException("User with email already exists!"))
+                        Mono.<User> error(new EmailAlreadyExistsException("User with email already exists!"))
                         )
-                .switchIfEmpty(repo.save(user));
+                .switchIfEmpty(
+                        Mono.defer( () -> {
+                            User user =  User.builder()
+                                    .email(dto.getEmail())
+                                    .password(passwordEncoder.encode(dto.getPassword()))
+                                    .name(dto.getName())
+                                    .role(dto.getRole())
+                                    .build();
+                            return repo.save(user);
+                        })
+                );
     }
 
     public Mono<User> findByEmail(String email){
